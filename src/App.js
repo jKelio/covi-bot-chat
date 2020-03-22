141b8d0a452012b2f4abb75d0e06a3cc1b803ad4
Leon Jäkel <leon.jaekel@sparqs.io>
2020-03-22 20:46:25 +0100

    Add custom styling like different colors for message bubbles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
 
+import { Card, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
 import React, { useMemo } from 'react';
 
-// import logo from './logo.svg';
+import logo from './covibot.jpg';
+import hackathon from './wirvsvirus.png';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    height: '95%',
+  },
+  left: {
+    textAlign: 'left'
+  },
+  right: {
+    textAlign: 'right'
+  },
+  media: {
+    height: 150,
+  }
+}));
+
+const styleOptions = {
+  bubbleBackground: '#5A9BD5',
+  bubbleFromUserBackground: '#88efa0',
+  botAvatarInitials: 'CV',
+};
 
 function App() {
-  const directLine = useMemo(() => createDirectLine({ token: window.secret }), []);
+  const classes = useStyles();
+  const directLine = useMemo(() => createDirectLine({
+    token: window.secret
+  }), []);
   const store = createStore({}, ({ dispatch }) => next => action => {
     if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
       dispatch({
@@ -20,7 +50,23 @@ function App() {
     return next(action);
   });
 
-  return <ReactWebChat directLine={directLine} store={store} />;
+  return <div className={classes.root}>
+    <Grid className={classes.container} container spacing={1}>
+      <Grid item xs={6}>
+        <Card className={classes.left}>
+          <img src={logo} alt={'Covi-Bot'} className={classes.media} />
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <Card className={classes.right}>
+          <img src={hackathon} alt={'Wir vs. Virus Hackathon'} className={classes.media} />
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <ReactWebChat directLine={directLine} store={store} styleOptions={styleOptions} />
+      </Grid>
+    </Grid>
+  </div>;
 }
 
 export default App;
