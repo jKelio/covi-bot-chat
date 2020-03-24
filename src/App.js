ac10f91c4c61e09b933e795b84690bb218a6a60a
Leon Jäkel <leon.jaekel@sparqs.io>
2020-03-24 20:31:22 +0100

    - change styling and fix firefox issue - add typing indicator to webchat

--- src/App.js
+++ src/App.js
@@ -1,66 +1,37 @@
 import './App.css';
 
-import { Card, Grid } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
-import React, { useMemo } from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react';
 
-import logo from './covibot.jpg';
 import hackathon from './wirvsvirus.png';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
   container: {
     height: '100%',
   },
   header: {
-    textAlign: 'center'
+    textAlign: 'center',
+    'background-color': 'gray',
   },
   media: {
-    height: 97,
+    height: 50,
   }
 }));
 
-const styleOptions = {
-  bubbleBackground: 'lightblue',
-  bubbleFromUserBackground: 'lightgreen',
-  botAvatarInitials: 'CV',
-  botAvatarImage: logo,
-  hideUploadButton: true,
-};
 
 function App() {
   const classes = useStyles();
-  const directLine = useMemo(() => createDirectLine({
-    token: window.secret
-  }), []);
-  const store = createStore({}, ({ dispatch }) => next => action => {
-    if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
-      dispatch({
-        type: 'WEB_CHAT/SEND_EVENT',
-        payload: {
-          name: 'webchat/join',
-          value: { language: window.navigator.language }
-        }
-      });
-    }
-    return next(action);
-  });
 
-  return <div className={classes.root}>
-    <Grid className={classes.container} container spacing={1}>
-      <Grid item xs={12}>
-        <Card className={classes.header}>
-          <img src={hackathon} alt={'Wir vs. Virus Hackathon'} className={classes.media} />
-        </Card>
-      </Grid>
-      <Grid id={'chat'} item xs={12}>
-        <ReactWebChat directLine={directLine} store={store} styleOptions={styleOptions} />
-      </Grid>
-    </Grid>
-  </div>;
+  return <AppBar position="static">
+    <Toolbar className={classes.header}>
+      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <img src={hackathon} alt={'Wir vs. Virus Hackathon'} className={classes.media} />
+    </Toolbar>
+  </AppBar>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import './index.css';
 
+import ReactWebChat, { createDirectLine, createStore } from 'botframework-webchat';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import logo from './covibot.jpg';
 import * as serviceWorker from './serviceWorker';
+
+const styleOptions = {
+  bubbleBackground: 'lightblue',
+  bubbleFromUserBackground: 'lightgreen',
+  botAvatarInitials: 'CV',
+  botAvatarImage: logo,
+  hideUploadButton: true,
+};
+
+const store = createStore({}, ({ dispatch }) => next => action => {
+  if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+    dispatch({
+      type: 'WEB_CHAT/SEND_EVENT',
+      payload: {
+        name: 'webchat/join',
+        value: { language: window.navigator.language }
+      }
+    });
+  }
+
+  return next(action);
+});
 
 fetch('https://covionestopbot.azurewebsites.net/api/environment').then((response) => {
   return response.json();
@@ -13,6 +37,9 @@ fetch('https://covionestopbot.azurewebsites.net/api/environment').then((response
   ReactDOM.render(
     <React.StrictMode>
       <App />
+      <ReactWebChat directLine={createDirectLine({
+        token: data.directLineSecret
+      })} store={store} sendTypingIndicator={true} styleOptions={styleOptions} />
     </React.StrictMode>,
     document.getElementById('root')
   );
